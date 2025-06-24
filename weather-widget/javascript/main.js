a06c553bcdfbf360110d1324cacfdbe86c6d8df3
drmimi47 <161758328+drmimi47@users.noreply.github.com>
2025-06-24 04:15:36 +0000

    see if this works

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,1 +1,15 @@
 console.log("this works");
+
+ const API_KEY = config.WEATHER_API_KEY;
+
+alert("hello, world!");
+
+
+
+  function getWeatherData() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10026&APPID=dc608cd922757b9bbf08cda5574e61c9`)
+  .then(response => response.json())
+  .then(data => console.log(data));
+  }
+
+  getWeatherData()
